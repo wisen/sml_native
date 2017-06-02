58e88427becbc8e3ddac6b52605fedab18d76251
wisen_wang <wisen.wang.ah@gmail.com>
2017-06-02 17:23:49 +0800

    no comment

--- Android.mk
+++ Android.mk
@@ -3,10 +3,9 @@ ifeq ($(RLK_ATRACE_MONITOR_SUPPORT),yes)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE := atrace_monitor
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES := bin/atrace_monitor
 include $(BUILD_PREBUILT)
-endif
 
+endif 
