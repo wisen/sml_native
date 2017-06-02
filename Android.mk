c5a574487869c7bc8667cb0a6d75aa0596300c68
wisen_wang <wisen.wang.ah@gmail.com>
2017-06-02 18:03:56 +0800

    no comment

--- Android.mk
+++ Android.mk
@@ -1,11 +1,35 @@
-ifeq ($(RLK_ATRACE_MONITOR_SUPPORT),yes)
-
-LOCAL_PATH := $(call my-dir)
-
+LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := atrace_monitor
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES := bin/atrace_monitor
-include $(BUILD_PREBUILT)
 
-endif 
+LOCAL_SRC_FILES:= atrace_monitor.c atrace_impl.c bugreport_impl.c common.c conn.c logcat_impl.c
+
+LOCAL_MODULE:= atrace_monitor
+
+LOCAL_MODULE_TAGS:= optional
+
+LOCAL_SHARED_LIBRARIES := \
+    libbinder \
+    libcutils \
+    libutils \
+    libz \
+
+#LOCAL_INIT_RC := atrace.rc
+
+include $(BUILD_EXECUTABLE)
+
+######
+#include $(CLEAR_VARS)
+
+#LOCAL_SRC_FILES := conn.c
+
+#LOCAL_MODULE := connj
+
+#LOCAL_MODULE_TAGS := optional
+
+#LOCAL_SHARED_LIBRARIES := \
+    libbinder \
+    libcutils \
+    libutils \
+    libz \
+
+#include $(BUILD_EXECUTABLE)
---
+++ Android_binary.mk
@@ -0,0 +1,11 @@
+ifeq ($(RLK_ATRACE_MONITOR_SUPPORT),yes)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := atrace_monitor
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := bin/atrace_monitor
+include $(BUILD_PREBUILT)
+
+endif 
